f2d11e217952c9decc9369f32040c6b22ff4f9db
Zeb Pykosz <zeb.apy@gmail.com>
2017-02-24 10:42:58 -0500

    dynamically set header padding top to push content down that would otherwise be under header

--- _js/main.js
+++ _js/main.js
@@ -6,6 +6,10 @@ require('./no.js');
 require('./decision-tree');
 require('./modals');
 
+const header = document.querySelector('.site-header');
+const headerHeight = header.offsetHeight + 'px';
+
+document.body.style.paddingTop = headerHeight;
 // quickly hide body when escape button is clicked
 document.querySelector('.js-escape-button').onclick = () => {
   document.querySelector('body').style.display = 'none';

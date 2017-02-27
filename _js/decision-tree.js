60b245cf1ba5fba7c2223b1403a9e068f90ac446
Zeb Pykosz <zeb.apy@gmail.com>
2017-02-27 16:07:30 -0500

    add back button to discovery

--- _js/decision-tree.js
+++ _js/decision-tree.js
@@ -2,29 +2,63 @@ const forEach = require('./forEach');
 
 const questions = document.querySelectorAll('.discovery__item');
 const answerLinks = document.querySelectorAll('.answers-list__link');
+const backButton = document.querySelector('.js-discovery-back-button');
+
+const activeClass = 'discovery__item--is-open';
+
+const previousIds = [];
+
+let activeId = '';
+
+function handleBackButtonClick(e) {
+  e.preventDefault();
+
+  const id = previousIds.pop();
+
+  if(id) {
+    showQuestion(id);
+  }
+}
 
 function hideQuestions() {
   forEach(questions, item => {
-    item.style.display = 'none';
+    item.classList.remove(activeClass);
   });
 }
 
 function showQuestion(id) {
+  hideQuestions();
+
   const elem = document.getElementById(id);
-  if(elem) {
-    elem.style.display = 'block';
-    window.scrollTo(0, 0);
+
+  if(!elem) {
+    return;
   }
+
+  document.body.classList.remove(`discovery-item-${activeId}`);
+  document.body.classList.add(`discovery-item-${id}`);
+
+  activeId = id;
+
+  elem.classList.add(activeClass);
+
+  window.scrollTo(0, 0);
 }
 
 function handleAnswerClick(e) {
   e.preventDefault();
+
   const { href } = e.target;
-  hideQuestions();
-  showQuestion(href.split('#')[1]);
+  const id = href.split('#')[1];
+
+  previousIds.push(activeId);
+
+  showQuestion(id);
 }
 
 function main() {
+  document.body.classList.add('has-discovery');
+
   if(!answerLinks.length || !questions.length) {
     return false;
   }
@@ -33,8 +67,11 @@ function main() {
     link.addEventListener('click', handleAnswerClick);
   });
 
-  hideQuestions();
-  showQuestion(questions[0].id);
+  const firstId = questions[0].id;
+  showQuestion(firstId);
+
+  backButton.addEventListener('click', handleBackButtonClick);
+
 }
 
 main();

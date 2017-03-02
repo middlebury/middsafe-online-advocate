94af80349683916e3ddbb9830e17cbcf86849112
Zeb Pykosz <zeb.apy@gmail.com>
2017-03-02 09:21:08 -0500

    place modal off screen to left before animating open

--- _js/modals.js
+++ _js/modals.js
@@ -47,6 +47,8 @@ function openModal(id) {
   // set the modal `is-top` class so it can be layed over any existing modals via css z-index
   modal.classList.add(modalTopClass);
 
+  modal.style.transform = 'translateX(-100vw)';
+
   // animate the modal open from the left based on the modal's width
   anime({
     targets: modal,
